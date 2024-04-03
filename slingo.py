bdc0cde80d43896424c4a92e20f40e17ea74477e
Maggie <157690330+maggiiezhang@users.noreply.github.com>
2024-04-03 13:04:19 -0400

    I added an incomplete parse_args() function, still needs to include paser.add_argument()

--- slingo.py
+++ slingo.py
@@ -1,5 +1,12 @@
+from argparse import ArgumentParser
+
 #Main project file
 def main():
     print("hello world")
     print("added this code")
 main()
+
+#Parse command-line arguments.
+def parse_args(arglist):
+    parser = ArgumentParser()
+    return parser.parse_args(argslist)
