9bd65a3729e345770eddfa935b8d0daba5c16ef2
Egypt Butler <egyptbutler02@gmail.com>
2024-04-04 13:45:00 -0400

    added Game class and an empty __init__ method

--- slingo.py
+++ slingo.py
@@ -1,5 +1,10 @@
 from argparse import ArgumentParser
 
+#class named 'Game' that will implement Slingo
+class Game():
+    def __init__(self) -> None:
+        pass
+    
 #Main project file
 def main():
     print("hello world")
@@ -10,3 +15,4 @@ main()
 def parse_args(arglist):
     parser = ArgumentParser()
     return parser.parse_args(argslist)
+
